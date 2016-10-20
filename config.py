1804439b186c66c24257e1017ee7997c1e2b6df0
lvpeschke <lena.peschke@student.uclouvain.be>
2016-10-20 23:22:07 +0200

    Set config parameters: entire server IP addrs

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # IP address of the proxy/server (either str or a list of str)
 IP_PROXY = ['10.0.11.42', '10.0.22.42']
 # Prefix of the IP addres of the proxy/server (either str or a list of str)
-PREFIX_IP_PROXY = '10.0.'
+PREFIX_IP_PROXY = ['10.0.11.42', '10.0.22.42']
 # Prefix of IPs belonging to WiFi access points
 PREFIX_IP_WIFI = 'none'
 # The password of SOCKS sessions
